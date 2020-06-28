111037fb6a377248de6d01e7ee0d192b81a629c8
xingxinglv <lvxingxing@kingsoft.com>
2020-06-28 11:29:23 +0800

    modify click

---
+++ app/src/main/java/com/dream/preferencektdemo/SActivity.kt
@@ -0,0 +1,12 @@
+package com.dream.preferencektdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a_layout)
+    }
+}
--- app/src/main/java/com/dream/preferencektdemo/SyncFragment.kt
+++ app/src/main/java/com/dream/preferencektdemo/SyncFragment.kt
@@ -1,10 +1,18 @@
 package com.dream.preferencektdemo
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SyncFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.sync_oreferences, rootKey)
+
+        val clickPre = findPreference<Preference>("clickpre")
+        clickPre?.setOnPreferenceClickListener {
+            Toast.makeText(context,"click---",Toast.LENGTH_LONG).show()
+            true
+        }
     }
 }
