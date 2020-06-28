14757582a0953af132da3a64fc20cda5fabfc1ab
xingxinglv <lvxingxing@kingsoft.com>
2020-06-28 11:51:30 +0800

    modify read value

--- app/src/main/java/com/dream/preferencektdemo/SyncFragment.kt
+++ app/src/main/java/com/dream/preferencektdemo/SyncFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 class SyncFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -14,5 +15,10 @@ class SyncFragment : PreferenceFragmentCompat() {
             Toast.makeText(context,"click---",Toast.LENGTH_LONG).show()
             true
         }
+
+        val preManager = PreferenceManager.getDefaultSharedPreferences(context)
+        val clickpre = preManager.getBoolean("notifications",false)
+        Toast.makeText(context,"value = $clickpre",Toast.LENGTH_LONG).show()
+
     }
 }
